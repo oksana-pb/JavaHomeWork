3c7588d47118be5cfe3a213d0c0525d478b12f83
oksana-pb <oksana.fesenko.01@privatbank.ua>
2021-10-25 23:38:21 +0300

    added new logic

--- src/com/pb/fesenko/hw3/Bingo.java
+++ src/com/pb/fesenko/hw3/Bingo.java
@@ -1,38 +1,46 @@
 package com.pb.fesenko.hw3;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Bingo {
 
     public static void main(String[] args) {
-        System.out.println("Вгадайте число від 0 до 100:");
+
+        System.out.println("Задумане число для гри в діапазоні від 0 до 100: ");
+
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+
+        System.out.println("Завдання!" +
+                "Вгадайте задумане число від 0 до 100:");
         System.out.println("Для виходу з програми введіть - 111");
 
 
         final int MAX_ATTEMPT = 101;
         int attempt = 0;
-        Scanner scan = new Scanner(System.in);
+
 
 
         while (attempt < MAX_ATTEMPT) {
             attempt++;
 
-            int chyslo = scan.nextInt();
 
+        int chyslo = scan.nextInt();
             if (chyslo == 111)
             {
                 System.out.println("Вихід з гри.");break;
             }
 
-            if (chyslo != 30 && chyslo < 30)
+            if (chyslo != x && chyslo < x)
             {
                 System.out.println("Введене число менше задуманого  - " + attempt + " спроба");
             }
-            if (chyslo != 30 && chyslo > 30)
+            if (chyslo != x && chyslo > x)
             {
                 System.out.println("Введене число більше задуманого - " + attempt + " спроба");
             }
-            if (chyslo == 30)
+            if (chyslo == x)
             {
                 System.out.println("Вітаю, ви вгадали з " +attempt+ " спроби!");
             }
